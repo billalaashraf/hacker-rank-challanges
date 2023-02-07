0e74116c884cb07aed29694e7d38a749cdc89ec1
Bilal Ashraf <billalaashraf@gmail.com>
2023-02-07 18:51:37 +0500

    last word lenght better solution

--- src/LeetCode/lengthOfLastWord.ts
+++ src/LeetCode/lengthOfLastWord.ts
@@ -1,6 +1,17 @@
 function lengthOfLastWord(s: string): number {
-    const arr = s.trim().split(" ");
-    return arr[arr.length - 1].length;
+    const n = s.length;
+    let wordlength = 0;
+    let wordfound = false;
+    for (let i = n - 1; i >= 0; i--) {
+        if (s[i] !== ' ') {
+            wordfound = true;
+            wordlength++;
+            continue;
+        } else if (wordfound) {
+            break;
+        }
+    }
+    return wordlength;
 }
 
 export default lengthOfLastWord;
